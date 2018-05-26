53f746cab039c824ac188de40b8827cdc2a0286c
PaulienBuskens <32159110+PaulienBuskens@users.noreply.github.com>
2018-05-26 17:04:32 +0200

    setting up home page

---
+++ toDo_app/resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<head>
+    <title>Laravel Todo application</title>
+    <link rel="stylesheet" type="text/css" href="{{url('css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('css/solarBootswatch.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{url('css/style.css')}}">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">To do App</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+    </ul>
+
+</nav>
+<footer id="footer" class= "text text-center">
+      <p>2018 Paulien Buskens</p>
+  </footer>
+</body>
+</html>
